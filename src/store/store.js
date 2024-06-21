c4b868562e0bf4318480cb55a1dcb3747617a7be
Moazzam Akmal <moazzamakmal@Moazzam-Akmal.local>
2024-06-21 17:52:51 +0500

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+ import Vuex from 'vuex';
+ import Vue from 'vue';
+ import todos from './modules/todos.js'  
+ 
+
+ Vue.use(Vuex);
+
+
+ export default new Vuex.Store({
+    modules : {
+        todos
+    } 
+ })
